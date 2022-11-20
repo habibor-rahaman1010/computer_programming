c3eae9cb3659fc873a83fdab6421e11ee6d87d7d
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-20 12:13:51 +0600

    ok

--- program-101.c
+++ program-101.c
@@ -1,9 +1,13 @@
-// function in c program...
+// sum 2 real number in c program...
 
 #include <stdio.h>
 
 int main() {
-
+    double a, b, c;
+    a = 2.5;
+    b = 4.6;
+    c  = a + b;
+    printf("%lf", c);
 
 return 0;
 }
---
+++ program-103.c
@@ -0,0 +1,18 @@
+//sum 2 real number in c program use function...
+
+#include <stdio.h>
+
+double add(double num1, double num2);
+
+int main() {
+    double a = 2.5, b = 5.7;
+    double result = add(a, b);
+    printf("%lf", result);
+
+return 0;
+}
+
+double add(double num1, double num2) {
+    double sum = num1 + num2;
+    return sum;
+}
