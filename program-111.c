9b86be32a628b6d2b9e1c4e003786957fa31ba3b
Web-Developer-Hub <habibor144369@gmail.com>
2022-11-20 13:49:07 +0600

    ok done

--- program-111.c
+++ program-111.c
@@ -1,1 +1,15 @@
+// area of a circle in c program...
 
+#include<stdio.h>
+int main() {
+    float r, Area,  Perimeter;
+    printf("Enter the radius of circle: ");
+    scanf("%f", &r);
+
+    Area = (3.14 * r * r);
+    Perimeter = (2 * 3.14 * r);
+
+    printf("Aria of the circle is: %0.2f \n", Area);
+    printf("perimeter of the circle is: %0.2f \n", Perimeter);
+    return 0;
+}
